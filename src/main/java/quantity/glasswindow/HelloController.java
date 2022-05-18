cc2c0bd6bd128f0807425fcfa66b706f904ca96e
VicentGJ <vsgarofalojerez@gmail.com>
2022-05-17 23:38:16 -0400

    feat: first transition implemented

--- src/main/java/quantity/glasswindow/HelloController.java
+++ src/main/java/quantity/glasswindow/HelloController.java
@@ -1,14 +1,23 @@
 package quantity.glasswindow;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
-public class HelloController {
+import java.io.IOException;
+
+public class HelloController extends TransitionController{
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
+    protected void onHelloButtonClick(ActionEvent event) throws IOException {
         welcomeText.setText("Welcome to JavaFX Application!");
+        this.transition("Login.fxml", event);
     }
 }
---
+++ src/main/java/quantity/glasswindow/TransitionController.java
@@ -0,0 +1,23 @@
+package quantity.glasswindow;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TransitionController {
+
+    @FXML
+    protected void transition(String fxmlResource, ActionEvent event) throws IOException {
+        Parent next_page_parent = FXMLLoader.load(getClass().getResource(fxmlResource));
+        Scene next_page_scene = new Scene(next_page_parent);
+        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.setScene(next_page_scene);
+        app_stage.show();
+    }
+}
