08958afa5688b41cf0d2775770100ec7c0252cad
VicentGJ <vsgarofalojerez@gmail.com>
2022-05-19 00:54:01 -0400

    Cleaning of dependencies and maven package configuration

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,6 @@
 module quantity.glasswindow {
     requires javafx.controls;
     requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
     requires java.logging;
 
     opens quantity.glasswindow to javafx.fxml;
