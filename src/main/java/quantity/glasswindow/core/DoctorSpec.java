f55db54d0ebed06c5c2a80190fa7b0774a6443e8
carlos-err406 <carlosvilaseca406@gmail.com>
2022-06-12 16:52:55 -0400

    feat: added th new enums for additional profile info

---
+++ src/main/java/quantity/glasswindow/core/DoctorSpec.java
@@ -0,0 +1,24 @@
+package quantity.glasswindow.core;
+
+public enum DoctorSpec {
+    ALLERGIST,
+    IMMUNOLOGIST,
+    DERMATOLOGIST,
+    RADIOLOGIST,
+    EMERGENCY_MED,
+    FAMILY_MED,
+    GENETICS,
+    NEUROLOGIST,
+    GYNECOLOGIST,
+    OPHTHALMOLOGIST,
+    PATHOLOGIST,
+    PREVENTIVE_MED,
+    REHAB_MED,
+    PSYCHIATRIST,
+    SURGEON,
+    UROLOGIST,
+
+
+
+
+}
---
+++ src/main/java/quantity/glasswindow/core/EngineerSpec.java
@@ -0,0 +1,12 @@
+package quantity.glasswindow.core;
+
+public enum EngineerSpec {
+    CIVIL,
+    INDUSTRIAL,
+    BIOMEDIC,
+    TELECOMUNICATIONS,
+    INFORMATIC,
+    AUTOMATIC,
+    ELECTRONIC,
+    MECHANIC
+}
