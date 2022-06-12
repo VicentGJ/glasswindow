e0f99c45a1b0037307ad1285a5589e9237bc238f
carlos-err406 <carlosvilaseca406@gmail.com>
2022-06-12 16:52:55 -0400

    feat: added th new enums for additional profile info

---
+++ src/main/java/quantity/glasswindow/core/ProgrammingLang.java
@@ -0,0 +1,20 @@
+package quantity.glasswindow.core;
+
+public enum ProgrammingLang {
+    PYTHON,
+    C,
+    CPP,
+    CSHARP,
+    JAVA,
+    GDSCRIPT,
+    PHP,
+    HTML,
+    CSS,
+    JS,
+    RUBY,
+    PERL,
+    RUST,
+    TYPESCRIPT,
+    SQL
+
+}
---
+++ src/main/java/quantity/glasswindow/core/ScientistSpec.java
@@ -0,0 +1,21 @@
+package quantity.glasswindow.core;
+
+public enum ScientistSpec {
+    ECOLOGY,
+    BIOLOGY,
+    CHEMISTRY,
+    MATHEMATICIAN,
+    MEDICAL_SCI,
+    PSYCHOLOGY,
+    GEOLOGY,
+    PALEONTOLOGY,
+    STATISTIC,
+    PHARMACOLOGY,
+    NEUROSCIENCE,
+    COMPUTER_SCI,
+    OCEANOGRAPHY,
+    MICROBIOLOGY,
+    PHYSICS,
+    FORENSIC,
+
+}
