11bb1312cbee9de07045e80361ecc898ab905cab
carlos-err406 <carlosvilaseca406@gmail.com>
2022-06-21 14:52:49 -0400

    feat:candidate id validation, uml updated

--- src/main/java/quantity/glasswindow/core/Candidate.java
+++ src/main/java/quantity/glasswindow/core/Candidate.java
@@ -1,6 +1,7 @@
 package quantity.glasswindow.core;
 
 import java.util.ArrayList;
+import java.util.regex.*;
 
 public class Candidate extends Model {
     private String name;
@@ -10,7 +11,6 @@ public class Candidate extends Model {
     private Scholarship scholarship;
     private Specialty specialty;
     private Branch sector;
-
     private ArrayList<IAdditionalInfo> addtionalInfo;
     public Candidate(String id, String name, Gender gender, String address, String phone, Scholarship scholarship,
                      Specialty specialty, Branch sector) {
@@ -83,5 +83,54 @@ public class Candidate extends Model {
 
     public ArrayList<IAdditionalInfo> getAddtionalInfo() {
         return addtionalInfo;
+    }
+
+    @Override
+    public void setId(String id) {
+        boolean onlyNumbers = Pattern.matches("\\d",id);
+        if(id.length() == 11 && onlyNumbers){
+            boolean dateValid = dateValidationID(id.substring(0,6));//[0-1]Year, [2-3]Month, [4-5]Day, [6]Century
+            if(dateValid){
+                boolean genderValid = genderValidateID(id.charAt(9));
+            }
+        }
+    }
+
+    private boolean dateValidationID(String idDate){
+        boolean isValidYear = true;//no way to validate this with 2 digits
+        boolean isValidMonth = true;
+        boolean isValidDay = false;
+        boolean isValidCentury = true;//depends on year, so no way to validate this
+        //int year = Integer.parseInt(idDate.substring(0,1));
+        int month = Integer.parseInt(idDate.substring(2,3));
+        int day = Integer.parseInt(idDate.substring(4,5));
+        //int century = Integer.parseInt(String.valueOf(idDate.charAt(6)));
+        if(day > 0)
+            switch (month){
+                case 1:case 3:case 5:case 7:
+                case 8:case 10:case 12:
+                    if(day <= 31)
+                        isValidDay = true;
+                    break;
+                case 4:case 6:
+                case 9:case 11:
+                    if(day <= 30)
+                        isValidDay = true;
+                    break;
+                case 2:
+                    if(day <= 29)
+                        isValidDay = true;
+                    break;
+                default:
+                    isValidMonth = false;
+                    break;
+            }
+        return (isValidYear && isValidMonth && isValidDay && isValidCentury);
+    }
+
+    private boolean genderValidateID(char idGender){
+        int gender = Integer.parseInt(String.valueOf(idGender));
+        Gender idG = gender % 2 == 0? Gender.MASCULINE : Gender.FEMININE;
+        return this.getGender().equals(idG) || this.getGender() == null;
     }
 }
