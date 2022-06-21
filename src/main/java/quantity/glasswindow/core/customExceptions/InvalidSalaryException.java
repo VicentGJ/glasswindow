6a2abf586b85d117f9ff748c5bbebb114c5d4318
carlos-err406 <carlosvilaseca406@gmail.com>
2022-06-21 17:58:09 -0400

    feat: new Custom ExceptionsInvalidNameException, InvalidSalaryException

---
+++ src/main/java/quantity/glasswindow/core/customExceptions/InvalidSalaryException.java
@@ -0,0 +1,7 @@
+package quantity.glasswindow.core.customExceptions;
+
+public class InvalidSalaryException extends Exception{
+    public InvalidSalaryException(String message) {
+        super(message);
+    }
+}
