2e9e8e1c27ba85af75ef0a2590c2d53e4efe57d0
carlos-err406 <carlosvilaseca406@gmail.com>
2022-07-01 11:19:49 -0400

    feats

--- src/main/java/quantity/glasswindow/ui/CandidateProfileController.java
+++ src/main/java/quantity/glasswindow/ui/CandidateProfileController.java
@@ -3,11 +3,33 @@ package quantity.glasswindow.ui;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import quantity.glasswindow.core.Agency;
+import quantity.glasswindow.core.Candidate;
+import quantity.glasswindow.core.customExceptions.IdNotFoundException;
 
 import java.io.IOException;
 
 public class CandidateProfileController extends TransitionController{
+    @FXML
+    private Text name,id,gender, address, phone,scholarship,branch, speciality,yearsofexp;
+    private Candidate candidate;
+
+    public void loadViewInfo(String id) throws IdNotFoundException {
+        Agency agency = Agency.getInstance();
+        this.id.setText(candidate.getId());
+        candidate = (Candidate) agency.getObject(id);
+        name.setText(candidate.getName());
+        gender.setText(candidate.getGender().name());
+        scholarship.setText(candidate.getScholarship().name());
+        branch.setText(candidate.getSector().name());
+        speciality.setText(candidate.getSpecialty().name());
+        address.setText(candidate.getAddress());
+        yearsofexp.setText(String.valueOf(candidate.getYearsOfExp()));
+        phone.setText(candidate.getPhone());
+    }
+
     @FXML
     protected void onBackButton(ActionEvent event) {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
--- src/main/java/quantity/glasswindow/ui/JobPostController.java
+++ src/main/java/quantity/glasswindow/ui/JobPostController.java
@@ -3,11 +3,27 @@ package quantity.glasswindow.ui;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import quantity.glasswindow.core.Agency;
+import quantity.glasswindow.core.JobPost;
+import quantity.glasswindow.core.customExceptions.IdNotFoundException;
 
 import java.io.IOException;
 
 public class JobPostController extends TransitionController{
+    @FXML
+    private Text description,company,salary,branch;
+    private JobPost jp;
+
+    public void loadViewInfo(String id) throws IdNotFoundException {
+        Agency agency = Agency.getInstance();
+        jp = (JobPost) agency.getObject(id);
+        company.setText(jp.getCompany());
+        description.setText(jp.getDescription());
+        branch.setText(jp.getBranch().name());
+        salary.setText(String.valueOf(jp.getSalary()));
+    }
     @FXML
     protected void onBackButton(ActionEvent event) {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
