30c3a5d7cb2281b38ea46a6723426e8b6a706144
VicentGJ <vsgarofalojerez@gmail.com>
2022-07-07 17:25:10 -0400

    implemented table behavior in report3

--- src/main/java/quantity/glasswindow/ui/Report3Controller.java
+++ src/main/java/quantity/glasswindow/ui/Report3Controller.java
@@ -1,19 +1,25 @@
 package quantity.glasswindow.ui;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import quantity.glasswindow.core.Agency;
 import quantity.glasswindow.core.Company;
 import quantity.glasswindow.core.Interview;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.time.Month;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class Report3Controller implements Initializable {
@@ -21,10 +27,11 @@ public class Report3Controller implements Initializable {
     private ComboBox<String> companyComboBox, monthComboBox;
     @FXML private TableView tableView;
 
-    @FXML private TableColumn<Interview, String> interViewIDs, candidateIDs, dates;
+    @FXML private TableColumn interViewIDs, candidateIDs, dates;
 
-    private ObservableList<String> companyComboBoxItems, monthItems;
-    private ObservableList<Interview> interviewList;
+    private ObservableList<String> companyComboBoxItems = FXCollections.observableArrayList(),
+            monthItems = FXCollections.observableArrayList();
+    private ObservableList<Interview> interviewList = FXCollections.observableArrayList();
     private Agency agency = Agency.getInstance();
 
     @Override
@@ -38,10 +45,25 @@ public class Report3Controller implements Initializable {
         companyComboBox.setItems(companyComboBoxItems);
         monthComboBox.setItems(monthItems);
         interviewList.addAll(agency.getInterviewList());
+        interViewIDs.setCellValueFactory(
+                new PropertyValueFactory<Interview, String>("id")
+        );
+        candidateIDs.setCellValueFactory(
+                new PropertyValueFactory<Interview, String>("candidate")
+        );
+        dates.setCellValueFactory(
+                new PropertyValueFactory<Interview, LocalDate>("date")
+        );
+        tableView.setItems(interviewList);
     }
-
+    @FXML
     public void onCompanyComboBox (ActionEvent event) {
         String companyID = companyComboBox.getValue();
         System.out.println(companyID);
+    }
+    @FXML
+    public void onBackButton (ActionEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
     }
 }
