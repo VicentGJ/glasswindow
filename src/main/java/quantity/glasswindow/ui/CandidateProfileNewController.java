bf94d75e0de3c38ca9d0f74815a70f9696d68124
carlos-err406 <carlosvilaseca406@gmail.com>
2022-07-07 21:19:55 -0400

    feat: candidate new, need to implement

---
+++ src/main/java/quantity/glasswindow/ui/CandidateProfileNewController.java
@@ -0,0 +1,23 @@
+package quantity.glasswindow.ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class CandidateProfileNewController extends TransitionController{
+
+    @FXML
+    protected void onSaveButton(ActionEvent event) {
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+    @FXML
+    protected void onBackButton(ActionEvent event) throws IOException{
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+}
